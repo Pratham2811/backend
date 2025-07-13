6692894b6677efeeb35802a5f583a58d02a9015b
Prathamesh Madane <148894430+Pratham2811@users.noreply.github.com>
2025-07-14 00:08:31 +0530

    datagram using UDP

---
+++ Nodejsnetworking/server.js
@@ -0,0 +1,17 @@
+import dgram from "node:dgram";
+const socket=dgram.createSocket('udp4') //event emitter means we can add diffrent types of events on this like it gives us back event 
+// console.log(socket);//it has events 
+// console.log(dgram);
+// we have created the socket and will recive the events on it 
+// like this 
+socket.on("message",(a,b)=>{
+    console.log(a,b);
+    
+})
+
+//socket is object which is event emitter 
+socket.on('listening',()=>{
+    console.log("listening the port ");
+    
+})
+socket.bind()
